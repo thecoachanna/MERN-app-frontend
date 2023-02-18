37c1d90a0444ef4ecb5367153ac14a10d9ca8cbd
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-02-18 13:32:28 -0500

    small styling updates

--- src/components/items/ItemsIndex.js
+++ src/components/items/ItemsIndex.js
@@ -8,6 +8,12 @@ import { getAllItems } from "../../api/items";
 // need messages from autodismissalert directory
 import messages from "../shared/AutoDismissAlert/messages";
 
+const cardContainerStyle = {
+    display: 'flex',
+    flexFlow: 'row-wrap',
+    justifyContent: 'center'
+}
+
 // ItemsIndex will make a request to the API for all items
 // once it receives a response, display a card for each item
 const ItemsIndex = (props) => {
@@ -48,17 +54,18 @@ const ItemsIndex = (props) => {
   // once we have an array of items, loop over them
   // produce one card for every item
     const itemCards = items.map(item => (
-        <Card key={ item.id }>
+        <Card key={ item.id } style= {{ width: '30%', margin: 5}}>
             <Card.Header>{ item.fullTitle }</Card.Header>
             <Card.Body>
                 <Card.Text>
-                    <Link to={`/items/${item.id}`}>View { item.name }</Link>
+                    <Link to={`/items/${item.id}`} className='btn btn-info'>View { item.name }</Link>
                 </Card.Text>
             </Card.Body>
         </Card>
     ))
+    // return some jsx, a container with all of the itemCards
     return (
-        <div className="container-md">
+        <div className="container-md" style={ cardContainerStyle }>
             { itemCards }
         </div>
     )
